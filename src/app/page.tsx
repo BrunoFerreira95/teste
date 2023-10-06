b493d50b212992fc554a43b68f88165ba09f9986
Bruno <brunoferreirapedraca@gmail.com>
2023-10-06 16:57:21 -0300

    fix: deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-import { Bruno, Instagram} from '../assets/export'
+import { Bruno, Instagram } from '../assets/export'
 import Link from "next/link";
 
 export default function Home() {
@@ -21,9 +21,10 @@ export default function Home() {
               </li>
             </ul>
           </header>
-          <main className="flex justify-center">
-            <div className='mt-20'>
-              <Image src={Bruno} className='bg-cover max-h-screen w-96' priority alt='bruno' />
+          <main className="flex justify-center ">
+            <div className='mt-40 bg-white p-5 rounded-lg'>
+
+              <Image src={Bruno} className='bg-cover max-h-screen w-96 rounded-lg' priority alt='bruno' />
 
             </div>
 
@@ -39,28 +40,47 @@ export default function Home() {
 
 
                 <p className=' text-black text-justify '>Bruno Ferreira Pedraça Biography:
+                </p><br/>
+                <p  className=' text-black text-justify '>
 
-                  Bruno Ferreira Pedraça is a talented musician and songwriter in the Rap-Hip-hop genre, with influences from R&B and Pop. Inspired by artists like Money Lola Young and Die for a Night Arizona, Bruno brings a unique and captivating approach to the current music scene.<br /><br />
+                  Bruno Ferreira Pedraça is a talented musician and songwriter in the Rap-Hip-hop genre, with influences from R&B and Pop. Inspired by artists like Money Lola Young and Die for a Night Arizona, Bruno brings a unique and captivating approach to the current music scene.
+                </p><br/>
+                <p  className=' text-black text-justify '>
 
-                  His musical journey began as a passionate listener, but soon the transcendent passion led him to want to share his own voice with the world. Bruno initially pursued studies in technical singing, but quickly realized that his true talent lay in expressing his creativity intuitively.<br /><br />
+                  His musical journey began as a passionate listener, but soon the transcendent passion led him to want to share his own voice with the world. Bruno initially pursued studies in technical singing, but quickly realized that his true talent lay in expressing his creativity intuitively.
+                </p><br/>
+                <p  className=' text-black text-justify '>
 
-                  Bruno's music is driven by his experience with mediumship, bringing lyrics that explore deep and emotional themes. His connection with the transcendental is reflected in compositions that touch the soul of listeners.<br /><br />
+                  Bruno's music is driven by his experience with mediumship, bringing lyrics that explore deep and emotional themes. His connection with the transcendental is reflected in compositions that touch the soul of listeners.
+                </p><br/>
+                <p  className=' text-black text-justify '>
 
-                  In addition to his vocal ability, Bruno also demonstrates talent in playing instruments, particularly in creating unique sounds amidst other instruments through his intuition. His search for sonic references that inspire him leads him to experiment with new arrangements and musical textures, resulting in an innovative and distinctive sound.<br /><br />
+                  In addition to his vocal ability, Bruno also demonstrates talent in playing instruments, particularly in creating unique sounds amidst other instruments through his intuition. His search for sonic references that inspire him leads him to experiment with new arrangements and musical textures, resulting in an innovative and distinctive sound.
+                </p><br/>
+                <p  className=' text-black text-justify '>
 
-                  Although he has no previous live performance experience, Bruno is determined to connect with his audience and take his music to the stage. His captivating presence and emotional delivery promise to create memorable moments during his future performances. <br /><br />
-                  Currently, Bruno is focused on releasing singles in order to gauge the reception of his work and carefully plan his next steps in his music career. His close connection to technology, combined with his background in the technological field, gives him a unique advantage in positioning himself as an independent musician and programmer.<br /><br />
+                  Although he has no previous live performance experience, Bruno is determined to connect with his audience and take his music to the stage. His captivating presence and emotional delivery promise to create memorable moments during his future performances.
+                </p><br/>
+                <p  className=' text-black text-justify '>
 
-                  Get ready to engage with the immersive musicality and reflective lyrics of Bruno Ferreira Pedraça. Follow him on social media to stay up to date with the latest news on his music releases and to explore the connections between music and technology on his artistic journey.<br /><br />
+                  Currently, Bruno is focused on releasing singles in order to gauge the reception of his work and carefully plan his next steps in his music career. His close connection to technology, combined with his background in the technological field, gives him a unique advantage in positioning himself as an independent musician and programmer.
+                </p><br/>
+                <p  className=' text-black text-justify '>
 
-                  For more information and press inquiries, please contact Bruno Ferreira Pedraça's management team at xxx-xxx.<br /><br />
+                  Get ready to engage with the immersive musicality and reflective lyrics of Bruno Ferreira Pedraça. Follow him on social media to stay up to date with the latest news on his music releases and to explore the connections between music and technology on his artistic journey.
+                </p><br/>
+                <p  className=' text-black text-justify '>
 
-                  Welcome an innovative artist who is ready to make his mark in the music industry - Bruno Ferreira Pedraça!<br /><br />
+                  For more information and press inquiries, please contact Bruno Ferreira Pedraça's management team at xxx-xxx.
+                </p><br/>
+                <p  className=' text-black text-justify '>
+
+                  Welcome an innovative artist who is ready to make his mark in the music industry - Bruno Ferreira Pedraça!
                 </p>
-                
+
               </div>
             </section>
-            
+
           </div>
         </div>
       </>
