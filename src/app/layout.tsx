7cce9835ebbc710b32f131baf3b827209da7a3f7
Bruno Ferreira Pedraça <47322135+BrunoFerreira95@users.noreply.github.com>
2023-12-08 20:10:55 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+              <!-- Google tag (gtag.js) -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-5QHCQ8X19X"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-5QHCQ8X19X');
+      </script>
+      </head>
       <body className={inter.className}>{children}<Analytics /></body>
     </html>
   )
