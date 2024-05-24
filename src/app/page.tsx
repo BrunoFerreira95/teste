74047a2af6f281805be427601794ffde5fed29aa
Bruno Ferreira <brunoferreirapedraca@gmail.com>
2024-05-24 06:40:01 -0300

    agora vai

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ function MusicIcon(props) {
   );
 }
 
-export default function Component() {
+export default async function Component() {
   return (
     <div className="flex flex-col min-h-[100dvh]">
       <header className="px-4 lg:px-6 h-14 flex items-center">
